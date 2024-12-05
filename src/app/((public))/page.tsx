b320ab13086ffbd817ff7cfe0d2348a601ac3376
Muhammad Abdullah <muhammadabdullah@Muhammads-MacBook-Pro-2.local>
2024-12-06 01:17:59 +0500

    improvements

--- src/app/((public))/page.tsx
+++ src/app/((public))/page.tsx
@@ -7,11 +7,12 @@ import UserProfileAnalytics from "./components/user.profile.analytics/user.profi
 import UserProfile from "./components/user.profile/user.profile";
 import RecommendedJobsListing from "./components/recommended.jobs.listing/recommended.jobs.listing";
 import FeaturedJobsListing from "./components/featured.jobs.listing/featured.jobs.listing";
+import { Divider } from "primereact/divider";
 
 export default function HomePage() {
   return (
     <main>
-      <div className="w-11 md:w-12 flex flex-wrap m-5 md:m-5 gap-3">
+      <div className="w-11 md:w-12 flex flex-wrap m-5  md:m-5 gap-3">
         <div className="w-12 md:w-3">
           <div className="flex gap-2 flex-column">
             <div className="w-12">
@@ -39,8 +40,11 @@ export default function HomePage() {
           <JobsAdditionalFilter />
           <AppTitle title="Featured Jobs" hrefTitle="See Featured Jobs" />
           <FeaturedJobsListing />
+          <Divider className="mt-3 mb-3" />
+          <div>
           <AppTitle title="Recommended Jobs" hrefTitle="See Recommended Jobs" />
           <RecommendedJobsListing />
+          </div>
         </div>
         <div></div>
       </div>
